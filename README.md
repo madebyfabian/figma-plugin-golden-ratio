5ff1462e73f2a33d0cda4f6ca2569d82574ad382
madebyfabian <beer.fabian@mail.de>
2020-04-10 09:22:55 +0200

    - Updated description - Updated figma.d.ts to V14 - Optimized code & production build

--- README.md
+++ README.md
@@ -1,28 +1,8 @@
 # figma-plugin-golden-ratio
-The Code for my Figma Plugin "Golden Ratio Align & Resize".
+You can find the plugin, a description, FAQs, and the version history at [figma.com/community/plugin/747059755185517110/](https://www.figma.com/community/plugin/747059755185517110/)
 
+### Feature requests? 😊 Questions?
+Just hit me on Twitter (@madebyfabian), via mail (hello@madebyfabian.com) or via an Issue.
 
-## Changelog
-### v1
-Written at Figma Plugins API Version 1.1.0 @ 24. August 2019
-
-### v2
-Updated description and added new feature roundBy8pxGrid @ 01. September 2019
-
-### v3
-Bugfix caused by v2 @ 01. September 2019
-
-### v4
-Corrected typo in description @ 01. September 2019
-
-### v5
-Added new Option to rotate the direction, Improved Round-by-8px Option, Added Support for Slice-Items, General performance Improvements @ 17. November 2019
-
-### v6
-Changed description
-
-### v7
-Changed API version to V12 and did a few general bugfixes & improvements. @ 16. February 2020
-
-### v8
-Bugfix when you click "Rotate direction" immediately after plugin launch.
+### Contributing
+If you want to, just fork this repo and create a PR if you like to add/improve something!
--- webpack.config.js
+++ webpack.config.js
@@ -8,63 +8,65 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // added by me
 const CopyPlugin = require('copy-webpack-plugin')
 
-module.exports = {
-  mode: (process.env.NODE_ENV === 'production') ? 'production' : 'development',
-  // force not using 'eval' which doesn't recognize __html__ and other globals
-  devtool: (process.env.NODE_ENV === 'production') ? '' : 'inline-source-map',
-  entry: {
-    main: './src/main.ts',
-    ui: './src/ui.js',
-  },
-  output: {
-    filename: '[name].js',
-    path: path.join(process.cwd(), './dist'),
-  },
-  module: {
-    rules: [
-      // JS and TS
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: [
-          'babel-loader',
-        ]
-      },
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: [
-          'babel-loader',
-          'ts-loader'
-        ]
-      },
-      // CSS and SCSS
-      {
-        test: /\.s?css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader',
-        ]
-      },
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.scss', '.html',],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({filename: '[name].css'}),
-    new HtmlWebpackPlugin({
-      filename: 'ui.html',
-      template: './src/ui.html',
-      inlineSource: '.(js|css)$',
-      chunks: ['ui'],
-    }),
-    new HtmlWebpackInlineSourcePlugin(),
-
-    // added by me
-    new CopyPlugin([
-      { from: './src/manifest.json', to: './manifest.json' },
-    ])
-  ],  
-};
+module.exports = (env, argv) => {
+  return {
+    mode: (argv.mode === 'production') ? 'production' : 'development',
+    // force not using 'eval' which doesn't recognize __html__ and other globals
+    devtool: (argv.mode === 'production') ? '' : 'inline-source-map',
+    entry: {
+      main: './src/main.ts',
+      ui: './src/ui.js',
+    },
+    output: {
+      filename: '[name].js',
+      path: path.join(process.cwd(), './dist'),
+    },
+    module: {
+      rules: [
+        // JS and TS
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: [
+            'babel-loader',
+          ]
+        },
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: [
+            'babel-loader',
+            'ts-loader'
+          ]
+        },
+        // CSS and SCSS
+        {
+          test: /\.s?css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+          ]
+        },
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.scss', '.html',],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({filename: '[name].css'}),
+      new HtmlWebpackPlugin({
+        filename: 'ui.html',
+        template: './src/ui.html',
+        inlineSource: '.(js|css)$',
+        chunks: ['ui'],
+      }),
+      new HtmlWebpackInlineSourcePlugin(),
+  
+      // added by me
+      new CopyPlugin([
+        { from: './src/manifest.json', to: './manifest.json' },
+      ])
+    ],  
+  }
+}
